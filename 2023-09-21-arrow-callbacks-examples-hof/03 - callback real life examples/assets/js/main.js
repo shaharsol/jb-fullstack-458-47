55fd539fc13bd439bcb8e172411be6366b512bce
zeev mindali <mindalizeev@gmail.com>
2023-09-21 20:20:35 +0200

    hints for exercise

--- 2023-09-21-arrow-callbacks-examples-hof/03 - callback real life examples/assets/js/main.js
+++ 2023-09-21-arrow-callbacks-examples-hof/03 - callback real life examples/assets/js/main.js
@@ -19,3 +19,17 @@ const notifyMe = () => {
     // setTimeout(alert(message), seconds * 1000) // (callback, when) when is measured in milliseconds
     setTimeout(() => alert(message), seconds * 1000) // (callback, when) when is measured in milliseconds
 } 
+
+// setTimeout(callback, when)
+// setInterval(callback, how-often)
+
+// example: this will log 'hi' in the console EVERY second
+// setInterval(() => console.log('hi'), 1000)
+
+// how to get current time in javascript
+// const now = new Date();
+// console.log(now.toLocaleTimeString());
+
+// your exercise:
+// display a clock on the page including seconds
+// hint: use setInterval
