cd35e1a248ef118490838aa13646170ffb9d582b
zeev mindali <mindalizeev@gmail.com>
2023-09-21 19:31:56 +0200

    const examples

--- 2023-09-21-arrow-callbacks-examples-hof/02 - arrow functions exercise/assets/js/main.js
+++ 2023-09-21-arrow-callbacks-examples-hof/02 - arrow functions exercise/assets/js/main.js
@@ -1,21 +1,46 @@
 function doSomething () {
     func1(function() { alert('hi'); })
-    func1(() => alert('hi'));
+    func1(() => alert('hi from arrow function'));
 
-    func2(function() { alert('hi'); return 'hello'; })
-    func3(function(x) { alert(x); })
+    func2(function() { alert('hi'); return 'hello'; });
+    func2(() => { alert('hi'); return 'hello'; });
+
+    func3(function(x) { alert(x); });
+    func3(x => alert(x));
+    func3(x => 42);
+
     func4(function(x, y) { alert(x + y); })
+    func4((x, y) => alert(x+y));
+
     func5(function(x, y , z) { alert(x * y * z); })
+    func5((x, y, z) => alert(x * y * z));
+
     func6(function() { return 42; })
+    func6(() => 42);
+
     func7(function(a, b) { return a + b; })
-    func8(function(a, b) { return a > b ? a : b; })
+    func7((a, b) => a + b); 
+
+    func8(function(a, b) { return a > b ? a : b; });
+    func8((a, b) => a > b ? a : b);
+
     func9(function(x, y , z) { alert(x); alert(y); alert(z);})
+    func9((x, y, z) => { alert(x); alert(y); alert(z);});
+
     func10(function() { return {name: 'betty', age: 1.5, color: 'gray'}; })
+    func10(() => ({name: 'betty', age: 1.5, color: 'gray'}))
 }
 
+const square = n => n**2;
+const square2 = function (n) {
+    return n**2;
+}
+
+console.log(square(3));
+
 // function() { alert('hi'); }
-function func1 (callback) {
-    callback();
+function func1 (cb) {
+    cb();
 }
 
 // function() { alert('hi'); return 'hello'; }
