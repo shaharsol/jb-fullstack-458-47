427add9dddad521e2a0694431a85a8c478340ee8
zeev mindali <mindalizeev@gmail.com>
2023-11-20 22:02:02 +0300

    refacto0r

--- 2023-11-20-rest/04 - carts/assets/js/main.js
+++ 2023-11-20-rest/04 - carts/assets/js/main.js
@@ -7,9 +7,15 @@ $(async () => {
             <td>${cart.userId}</td>
             <td>${cart.totalProducts}</td>
             <td>${cart.totalQuantity}</td>
-            <td><button>show products</button></td>
+            <td><button id="button-${cart.id}">show products</button></td>
         </tr>
     `).reduce((acc, curr) => acc + curr, '');
     document.getElementById('carts').innerHTML = html;
+
+    document.getElementById('carts').addEventListener('click', (e) => {
+        if(e.target.id.startsWith('button-')) {
+            const cartId = e.target.id.substring('button-'.length);
+        }
+    })
 
 });
