2fc008910e888725457b66e58a04d3670dd456ca
zeev mindali <mindalizeev@gmail.com>
2023-11-20 21:53:51 +0300

    carts table

---
+++ 2023-11-16-typescript-rest/04 - carts/assets/js/main.js
@@ -0,0 +1,14 @@
+(async () => {
+    const response = await fetch('https://dummyjson.com/carts');
+    const { carts } = await response.json();
+    const html = carts.map(cart => `
+        <tr>
+            <td>${cart.id}</td>
+            <td>${cart.userId}</td>
+            <td>${cart.totalProducts}</td>
+            <td>${cart.totalQuantity}</td>
+        </tr>
+    `).reduce((acc, curr) => acc + curr, '');
+    document.getElementById('carts').innerHTML = html;
+
+})();
