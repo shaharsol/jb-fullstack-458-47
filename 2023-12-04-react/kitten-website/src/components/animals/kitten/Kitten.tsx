e2652d2b0c098f62ef07050a5a9e92ba6369c405
zeev mindali <mindalizeev@gmail.com>
2023-12-04 20:19:25 +0300

    kitten image

--- 2023-12-04-react/kitten-website/src/components/animals/kitten/Kitten.tsx
+++ 2023-12-04-react/kitten-website/src/components/animals/kitten/Kitten.tsx
@@ -1,11 +1,14 @@
 import './Kitten.css';
+import KittenImageSource from '../../../assets/images/kitten.jpg';
 
 function Kitten(): JSX.Element {
     return (
         <div className="Kitten">
             <h1>Kitten</h1>
             <div>Cute Kitten:</div>
-            <div>testing2</div>
+            <img src={KittenImageSource} />
+            <br />
+            <span>Price: $50</span>
         </div>
     )
 }
