e7b4a15f6fc10d46a1e72fd8200a30669fdf4ec6
zeev mindali <mindalizeev@gmail.com>
2023-12-04 20:10:56 +0300

    react

---
+++ 2023-12-04-react/kitten-website/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Kitten from './components/animals/kitten/Kitten';
+
+function App() {
+    return (
+        <h1>Our First react app</h1>
+    );
+}
+
+export default App;
---
+++ 2023-12-04-react/kitten-website/src/components/animals/kitten/Kitten.tsx
@@ -0,0 +1,13 @@
+import './Kitten.css';
+
+function Kitten(): JSX.Element {
+    return (
+        <div className="Kitten">
+            <h1>Kitten</h1>
+            <div>Cute Kitten:</div>
+            <div>testing2</div>
+        </div>
+    )
+}
+
+export default Kitten;
