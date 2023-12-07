cdfabdbdc751a25a7b9c166585dbdd77cd5b1a2e
zeev mindali <mindalizeev@gmail.com>
2023-12-07 22:27:13 +0300

    upto axios

---
+++ 2023-12-07-react-state-routing/northwind/src/services/Products.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+import Product from "../models/product";
+import appConfig from "../utils/AppConfig";
+
+
+class Products {
+
+    public async getAll(): Promise<Product[]> {
+        // get the products from remote server
+        const response = await axios.get<Product[]>(appConfig.productsUrl);
+
+        // extract the data from the response
+        const products = response.data;
+
+        return products;
+    }
+}
+
+const products = new Products();
+export default products;
