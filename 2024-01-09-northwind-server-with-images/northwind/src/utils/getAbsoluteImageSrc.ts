8bbc32fd8ca4820cfa2061461151428f78cf1e13
zeev mindali <mindalizeev@gmail.com>
2024-01-15 22:04:14 +0300

    sql finished

--- 2024-01-09-northwind-server-with-images/northwind/src/utils/getAbsoluteImageSrc.ts
+++ 2024-01-09-northwind-server-with-images/northwind/src/utils/getAbsoluteImageSrc.ts
@@ -1,9 +1,15 @@
 import appConfig from "./AppConfig";
 
 function getAbsoluteImageSrc(src: string | undefined): string {
+    // from the rest api
+    // we will always get src as http://localhost:3030/something.jpg
     if (!src) return '';
     const originalUrl = new URL(src);
     return `${appConfig.apiBaseUrl}${originalUrl.pathname}`;
+    // dev:
+    // http://localhost:3030/something.jpg
+    // prod:
+    // https://murmuring-ocean-69355-1c3f77a1ae50.herokuapp.com/something.jpg
 }
 
 export default getAbsoluteImageSrc;
