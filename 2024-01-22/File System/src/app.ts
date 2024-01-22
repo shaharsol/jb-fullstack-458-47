00ddbc9c0b10ddd624fa77c5c8236079ba294fde
zeev mindali <mindalizeev@gmail.com>
2024-01-22 20:02:43 +0300

    finalized template

---
+++ 2024-01-22/File System/src/app.ts
@@ -0,0 +1,6 @@
+import os from 'os';
+import fs from 'fs';
+
+const str: string = 'hello world 3';
+
+console.log(str);
--- 2024-01-22/Node Typescript Template/src/app.ts
+++
@@ -1,6 +0,0 @@
-import os from 'os';
-import fs from 'fs';
-
-const str: string = 'hello world 3';
-
-console.log(str);
