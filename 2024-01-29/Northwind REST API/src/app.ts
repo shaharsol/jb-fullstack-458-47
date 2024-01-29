ef0394a5b828b5c9404dac1b9b6ded1392fb5790
Shahar Solomianik <shahar@gnift.com>
2024-01-29 21:33:06 +0300

    config

--- 2024-01-29/Northwind REST API/src/app.ts
+++ 2024-01-29/Northwind REST API/src/app.ts
@@ -1,10 +1,13 @@
+process.env['NODE_CONFIG_DIR'] = __dirname + '/config/';
+
 import express from "express";
 import productsRouter from './routers/products';
+import config from 'config';
 
 const server = express();
 
 server.use('/api/products', productsRouter)
 
-server.listen(8080, () => {
-    console.log('server is running on localhost:8080')
+server.listen(config.get<number>('app.port'), () => {
+    console.log(`${config.get<string>('app.name')} is running on localhost:${config.get<number>('app.port')}`)
 })
--- 2024-01-29/Northwind REST API/src/models/product/mysql.ts
+++ 2024-01-29/Northwind REST API/src/models/product/mysql.ts
@@ -1,19 +1,20 @@
+import query from "../../db/mysql";
 import DTO from "./dto";
 import Model from "./model";
 
 class Product implements Model {
     public async getAll(): Promise<DTO[]> {
-        return [
-            {
-                id: 1,
-                name: 'chai',
-                price: 6,
-                stock: 8
-            }
-        ]
+        const products = await query(`
+            SELECT  ProductID AS id,
+                    ProductName AS name,
+                    UnitPrice AS price,
+                    UnitsInStock AS stock
+            FROM    products
+        `)
+        return products;
     }
 
-    public async getOne(): Promise<DTO> {
+    public async getOne(id: number): Promise<DTO> {
         return {
                 id: 1,
                 name: 'chai',
