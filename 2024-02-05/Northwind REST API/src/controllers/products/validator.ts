7076a379aa1d8f6c90324fc71d9d5ecc095ee321
zeev mindali <mindalizeev@gmail.com>
2024-02-05 19:48:40 +0200

    put and patch

--- 2024-02-05/Northwind REST API/src/controllers/products/validator.ts
+++ 2024-02-05/Northwind REST API/src/controllers/products/validator.ts
@@ -6,3 +6,11 @@ export const addProductValidator = Joi.object<DTO>({
     price: Joi.number().min(1).max(1000).required(),
     stock: Joi.number().min(0).max(1000).default(0)
 });
+
+export const putProductValidator = addProductValidator;
+
+export const patchProductValidator = Joi.object<DTO>({
+    name: Joi.string().alphanum().min(4).lowercase(),
+    price: Joi.number().min(1).max(1000),
+    stock: Joi.number().min(0).max(1000)
+});
--- 2024-02-05/Northwind REST API/src/routers/products.ts
+++ 2024-02-05/Northwind REST API/src/routers/products.ts
@@ -1,7 +1,7 @@
 import express, { Router } from "express";
 import { getAll, getOne, add, update, patch, remove } from "../controllers/products/controller";
 import validate from "../middlewares/input-validation";
-import { addProductValidator } from '../controllers/products/validator';
+import { addProductValidator, patchProductValidator, putProductValidator } from '../controllers/products/validator';
 
 const router = Router();
 
@@ -9,8 +9,8 @@ const router = Router();
 router.get('/', getAll)
 router.get('/:id', getOne)
 router.post('/', validate(addProductValidator) , add)
-router.put('/:id', update)
-router.patch('/:id', patch)
+router.put('/:id', validate(putProductValidator) ,update)
+router.patch('/:id', validate(patchProductValidator), patch)
 router.delete('/:id', remove)
 
 export default router;
