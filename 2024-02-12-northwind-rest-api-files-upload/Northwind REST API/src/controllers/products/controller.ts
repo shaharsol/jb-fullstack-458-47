b34aa69aa938f2d6ba559ea8966c83515eeb1992
shaharsol <shahar@gnift.com>
2024-02-15 19:46:49 +0200

    convertProductToImageUrl

--- 2024-02-12-northwind-rest-api-files-upload/Northwind REST API/src/controllers/products/controller.ts
+++ 2024-02-12-northwind-rest-api-files-upload/Northwind REST API/src/controllers/products/controller.ts
@@ -3,6 +3,16 @@ import getModel from "../../models/product/factory"
 import { StatusCodes, ReasonPhrases } from 'http-status-codes';
 import createHttpError, { NotFound } from "http-errors";
 import config from 'config';
+import productDTO from "../../models/product/dto";
+
+function convertProductToImageUrl(product: productDTO) {
+    const productWithImageUrl = {
+        ...product,
+        imageUrl: `${config.get<string>('app.protocol')}://${config.get<string>('app.host')}:${config.get<number>('app.port')}/images/${product.imageName}`
+    }
+    delete productWithImageUrl.imageName;
+    return productWithImageUrl;
+}
 
 export const getAll = async (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -18,7 +28,7 @@ export const getOne = async (req: Request, res: Response, next: NextFunction) =>
     try {
         const product = await getModel().getOne(+req.params.id);
         if (!product) return next();
-        res.json({...product, image: 'https://www.budgetbytes.com/wp-content/uploads/2021/09/Honey-Sriracha-Tofu-close.jpg'});
+        res.json(convertProductToImageUrl(product));
     } catch (err) {
         next(err)
     }
@@ -27,12 +37,7 @@ export const getOne = async (req: Request, res: Response, next: NextFunction) =>
 export const add = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const product = await getModel().add(req.body);
-        const productWithImageUrl = {
-            ...product,
-            imageUrl: `${config.get<string>('app.protocol')}://${config.get<string>('app.host')}:${config.get<number>('app.port')}/images/${product.imageName}`
-        }
-        delete productWithImageUrl.imageName
-        res.status(StatusCodes.CREATED).json(productWithImageUrl);
+        res.status(StatusCodes.CREATED).json(convertProductToImageUrl(product));
     } catch (err) {
         next(err)
     }
@@ -43,7 +48,7 @@ export const update = async (req: Request, res: Response, next: NextFunction) =>
         const id = +req.params.id;
         const updatedProduct = {id, ...req.body}
         const product = await getModel().update(updatedProduct);
-        res.json(product);
+        res.json(convertProductToImageUrl(product));
     } catch (err) {
         next(err)
     }
@@ -56,7 +61,7 @@ export const patch = async (req: Request, res: Response, next: NextFunction) => 
         const existingProduct = await getModel().getOne(id);
         const updatedProduct = {...existingProduct, ...req.body};
         const product = await getModel().update(updatedProduct);
-        res.json(product);
+        res.json(convertProductToImageUrl(product));
     } catch (err) {
         next(err)
     }
